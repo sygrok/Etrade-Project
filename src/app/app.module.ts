3200cdebdedb83a88e871d2c6abf1247fe966b41
Berkay Anduv <sygrok@hotmail.com>
2023-01-10 20:40:02 +0300

    change-navbar&footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RegisterPageComponent } from './components/register-page/register-page.
 import { GuestNavbarComponent } from './layouts/guest-navbar/guest-navbar.component';
 import { FilterPipe } from './pipes/filter.pipe';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     RegisterPageComponent,
     GuestNavbarComponent,
     FilterPipe,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
